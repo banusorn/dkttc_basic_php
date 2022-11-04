5a268657792a581b0848769e20b2899c9b5bb6d4
642012800003 <106530632+Phachradanai@users.noreply.github.com>
2022-11-04 16:06:40 +0700

    4/11/2565

--- 64201280003/index.php
+++ 64201280003/index.php
@@ -1,4 +1,25 @@
-<?php
-    echo "Hello\tWorld";
-    echo "\tสวัดดีชาวโลก";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML & PHP</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        
+        echo "Hello PHP L";
+        echo "<h1>Ez. PHP L</1H>";
+    
+    
+    
+    
+    ?>
+</body>
+</html>
